92d484de4656a2cc626663aae68abd00606eb26e
isaacoduh <ufuomaoduh@gmail.com>
2020-02-19 03:38:08 +0100

    Added DrinkItem to category drinks screen

--- lib/category_drinks_screen.dart
+++ lib/category_drinks_screen.dart
@@ -1,3 +1,5 @@
+import 'package:drinksdemo/dummy_data.dart';
+import 'package:drinksdemo/widgets/drink_item.dart';
 import 'package:flutter/material.dart';
 
 class CategoryDrinksScreen extends StatelessWidget {
@@ -10,15 +12,20 @@ class CategoryDrinksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
-    final CategoryId = routeArgs['id'];
+    final categoryId = routeArgs['id'];
+    final categoryDrinks = DUMMY_DRINKS.where((drink) {
+      return drink.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The drinks for the category'
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index){
+          return DrinkItem(title: categoryDrinks[index].title, imageUrl: categoryDrinks[index].imageUrl, servetime: categoryDrinks[index].servetime);
+        },
+        itemCount: categoryDrinks.length,
       ),
     );
   }
