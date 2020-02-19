1501f896fc616f29495fe73b2efe3dfca47c7a7d
isaacoduh <ufuomaoduh@gmail.com>
2020-02-19 05:52:17 +0100

    Feature: Complete filter list page

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import '../models/drink.dart';
+import '../widgets/drink_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
+  final List<Drink> favoriteDrinks;
+
+  FavoritesScreen(this.favoriteDrinks);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('The Favorites'),
-    );
+    if (favoriteDrinks.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet - care to add some?'),
+      );
+    }else{
+      return ListView.builder(
+        itemBuilder: (context, index){
+          return DrinkItem(id: favoriteDrinks[index].id, title: favoriteDrinks[index].title, imageUrl: favoriteDrinks[index].imageUrl, servetime: favoriteDrinks[index].servetime);
+        },
+        itemCount: favoriteDrinks.length,
+      );
+    }
   }
 }
--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,12 +2,35 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  final Function saveFilters;
+  final Map<String, bool> currentFilters;
+
+  FiltersScreen(this.currentFilters, this.saveFilters);
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _gluttenFree = false;
+
+  @override
+  void initState() {
+    _gluttenFree = widget.currentFilters['glutten'];
+    super.initState();
+  }
+
+  Widget _builSwitchListTile(String title, String description, bool currentValue, Function updateValue){
+    return SwitchListTile(title: Text(title), value: currentValue, subtitle: Text(description), onChanged: updateValue,);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Your Filters'),),
+      
       drawer: MainDrawer(),
       body: Center(
         child: Text('Filters'),
