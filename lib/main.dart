92ff1768efbd394949c256ff1f5c10e04b065234
isaacoduh <ufuomaoduh@gmail.com>
2020-02-19 01:51:07 +0100

    Added Themeing config, Updated Categories Screen to show padding

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DrinkGuide',
       theme: ThemeData(
         primarySwatch: Colors.grey,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          body2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       home: CategoriesScreen(),
     );
