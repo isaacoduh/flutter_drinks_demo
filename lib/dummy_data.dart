e6987a220115a3655334c7fd8d4a2a46506ea289
isaacoduh <ufuomaoduh@gmail.com>
2020-02-19 01:04:40 +0100

    Initial Setup, Dummy Categories Data, Category Model, Category Item Widget, Category List Screen, Update Main

---
+++ lib/dummy_data.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'cat_1',
+    title: 'Amarena Cocktails',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'cat_2',
+    title: 'Aria&Velluto',
+    color: Colors.red
+  ),
+  Category(
+    id: 'cat_3',
+    title: 'Caffetteria',
+    color: Colors.orange
+  ),
+  Category(
+    id: 'cat_4',
+    title: 'Caffetteria Internazionale',
+    color: Colors.amber
+  ),
+  Category(
+    id: 'cat_5',
+    title: 'Alcoholic Cocktail',
+    color: Colors.blue
+  ),
+  Category(
+    id: 'cat_6',
+    title: 'Analcoholic Cocktail',
+    color: Colors.green
+  ),
+  Category(
+    id: 'cat_7',
+    title: 'Cocktail IBA',
+    color: Colors.lightBlue
+  ),
+  Category(
+    id: 'cat_8',
+    title: 'Cocktail Solido',
+    color: Colors.pink
+  ),
+  Category(
+    id: 'cat_9',
+    title: 'Coffee Specialty',
+    color: Colors.teal
+  ),
+  Category(
+    id: 'cat_10',
+    title: 'Cold Drink',
+    color: Colors.brown
+  ),
+  Category(
+    id: 'cat_11',
+    title: 'Frappè',
+    color: Colors.cyan
+  ),
+  Category(
+    id: 'cat_12',
+    title: 'Gelato Cocktail',
+    color: Colors.deepOrange
+  ),
+  Category(
+    id: 'cat_13',
+    title: 'Granita',
+    color: Colors.indigo
+  ),
+  Category(
+    id: 'cat_14',
+    title: 'Hot Drink',
+    color: Colors.limeAccent
+  ),
+  Category(
+    id: 'cat_15',
+    title: 'Italian soda',
+    color: Colors.redAccent
+  ),
+  Category(
+    id: 'cat_16',
+    title: 'Lemonade',
+    color: Colors.yellow
+  ),
+  Category(
+    id: 'cat_17',
+    title: 'Smoothie',
+    color: Colors.deepPurpleAccent
+  ),
+  Category(
+    id: 'cat_18',
+    title: 'Sorbet',
+    color: Colors.lightGreen
+  ),
+];
